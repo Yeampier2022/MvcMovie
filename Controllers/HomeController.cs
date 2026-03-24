b6b99a50ff63b8a4799fbe49ed0b360aa69a71f3
Yeampier2022 <yeampierjesusa@gmail.com>
2026-03-24 14:10:32 -0400

    feat: Part 3, add a view to an ASP.NET Core MVC app

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,21 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MvcMovie.Models;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers;
 
-public class HomeController : Controller
+public class HelloWorldController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    // 
+    // GET: /HelloWorld/
     public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult Welcome(string name, int numTimes = 5)
     {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
